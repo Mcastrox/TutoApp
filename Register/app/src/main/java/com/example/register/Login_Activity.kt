3158f5417e7245cfaba0d6027f2ea7510e495359
Mcastrox <00002018@uca.edu.sv>
2020-03-29 19:28:27 -0600

    aiuda

---
+++ Register/app/src/main/java/com/example/register/Login_Activity.kt
@@ -0,0 +1,59 @@
+package com.example.register
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.Toast
+import com.example.register.R.id.progressBar2
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login_.*
+import kotlinx.android.synthetic.main.activity_register.*
+
+class Login_Activity : AppCompatActivity() {
+    private lateinit var txtUser:EditText
+    private lateinit var txtPassword:EditText
+    private lateinit var progressBar: ProgressBar
+    private lateinit var auth: FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_)
+        txtUser=findViewById(R.id.txtUser)
+        txtPassword=findViewById(R.id.txt_password)
+        progressBar= findViewById(R.id.progressBar2)
+        auth= FirebaseAuth.getInstance()
+
+    }
+    fun forgotPassword(view:View){
+        startActivity(Intent(this,Forgotpass_Activity::class.java))
+    }
+    fun register(view:View){
+        startActivity(Intent(this,Register_Activity::class.java))
+    }
+    fun login (view: View){
+        loginUser()
+    }
+    private fun loginUser(){
+        val user: String=txtUser.text.toString()
+        val pass: String=txtPassword.text.toString()
+        if(!TextUtils.isEmpty(user)&&!TextUtils.isEmpty(pass) ){
+            progressBar.visibility=View.VISIBLE
+
+            auth.signInWithEmailAndPassword(user,pass)
+                .addOnCompleteListener(this){
+                        task->
+                    if(task.isSuccessful){
+                        action()
+                    }else{
+                        Toast.makeText(this,"Error al autenticar", Toast.LENGTH_LONG).show()
+                    }
+                }
+        }
+    }
+    private fun action(){
+        startActivity(Intent(this,MainActivity::class.java))
+    }
+}
---
+++ Register/app/src/main/java/com/example/register/Register_Activity.kt
@@ -0,0 +1,89 @@
+package com.example.register
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_register.*
+
+class Register_Activity : AppCompatActivity() {
+    private lateinit var txtName: EditText
+    private lateinit var txtLastname: EditText
+    private lateinit var txtEmail: EditText
+    private lateinit var txtPassword: EditText
+    private lateinit var progressBar: ProgressBar
+    private lateinit var dbReference: DatabaseReference
+    private lateinit var database: FirebaseDatabase
+    private lateinit var auth: FirebaseAuth
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        txtName=findViewById(R.id.txt_name)
+        txtLastname=findViewById(R.id.txt_lastname)
+        txtEmail=findViewById(R.id.txt_email)
+        txtPassword=findViewById(R.id.txt_password)
+
+        progressBar= findViewById(R.id.progressBar)
+
+        database= FirebaseDatabase.getInstance()
+        auth=FirebaseAuth.getInstance()
+        dbReference=database.reference.child("User")
+
+    }
+    fun register(view:View){
+        createNewUser()
+        startActivity(Intent(this,MainActivity::class.java))
+
+    }
+    private fun createNewUser(){
+        val name: String = txt_name.text.toString()
+        val lastName: String = txt_lastname.text.toString()
+        val email: String = txt_email.text.toString()
+        val pass: String = txt_password.text.toString()
+
+        if(!TextUtils.isEmpty(name)&&!TextUtils.isEmpty(lastName)&&!TextUtils.isEmpty(email)&&!TextUtils.isEmpty(pass)){
+            progressBar.visibility=View.VISIBLE
+            auth.createUserWithEmailAndPassword(email,pass)
+                .addOnCompleteListener(this){
+                        task->
+                    if(task.isComplete){
+                        val user:FirebaseUser?=auth.currentUser
+                        verifyEmail((user))
+
+                        val userBD=dbReference.child(user?.uid!!)
+                        userBD.child("Name").setValue(name)
+                        userBD.child("lastName").setValue(lastName)
+                        action()
+                    }
+                }
+        }
+    }
+    private fun action (){
+        startActivity(Intent(this,MainActivity::class.java))
+
+    }
+
+
+    private fun verifyEmail(user:FirebaseUser?){
+        user?.sendEmailVerification()
+            ?.addOnCompleteListener(this){
+                    task ->
+                if(task.isComplete){
+                    Toast.makeText(this,"Email enviado con exito",Toast.LENGTH_LONG).show()
+                }else{
+                    Toast.makeText(this,"error al enviar",Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+}
