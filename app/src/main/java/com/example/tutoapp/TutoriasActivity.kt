d34cce865c3ba9f49b8842d14d4ee132de16ba65
OsEstrada <00064318@uca.edu.sv>
2020-05-22 22:04:46 -0600

    modificado toolbar de TutoriasActivity

--- app/src/main/java/com/example/tutoapp/TutoriasActivity.kt
+++ app/src/main/java/com/example/tutoapp/TutoriasActivity.kt
@@ -4,10 +4,15 @@ import android.content.ClipData
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.get
+import androidx.databinding.DataBindingUtil
 
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.example.tutoapp.databinding.ActivityTutoriasBinding
 import com.example.tutoapp.viewmodel.TutorViewModel
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
@@ -15,22 +20,40 @@ import kotlinx.android.synthetic.main.activity_tutorias.*
 import kotlinx.android.synthetic.main.fragment_search.*
 
 class TutoriasActivity : AppCompatActivity() {
+
     private lateinit var auth: FirebaseAuth
     private lateinit var uidTutor: String
-
+    private lateinit var Toolbar: Toolbar
     private lateinit var adapter: SolicitudAdapter
+    private lateinit var binding: ActivityTutoriasBinding
 
     private val viewModel by lazy { ViewModelProvider(this).get(TutorViewModel::class.java) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tutorias)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_tutorias)
+        setSupportActionBar(binding.toolbar)
+
         getCurrentUser()
         observerData()
-
-
-
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        getMenuInflater().inflate(R.menu.solicitud_menu, menu);
+        return true;
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item?.itemId) {
+            R.id.next_page -> {
+                startActivity(Intent(this,AtributesActivity::class.java))
+                return true
+            }
+            else -> (return super.onOptionsItemSelected(item))
+        }
+    }
+
 
     private fun getCurrentUser() {
         auth = FirebaseAuth.getInstance()
@@ -43,7 +66,7 @@ class TutoriasActivity : AppCompatActivity() {
             adapter = SolicitudAdapter(this, it)
             lista_solicitudes.adapter = adapter
             lista_solicitudes.setOnItemClickListener { parent, view, position, id ->
-                val intent = Intent(this,VerSolicitudActivity::class.java)
+                val intent = Intent(this, VerSolicitudActivity::class.java)
                 intent.putExtra("solicitud", it[position])
                 startActivity(intent)
             }
