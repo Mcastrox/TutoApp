5c6b3e337f1e6b0816732fded46fb7549e461a0a
Ismael Garcia <00036818@uca.edu.sv>
2020-06-23 14:33:24 -0600

    funcionalidad btn toolbar de pseleccionado y tutorActivity

--- app/src/main/java/com/example/tutoapp/TutorActivity.kt
+++ app/src/main/java/com/example/tutoapp/TutorActivity.kt
@@ -3,6 +3,7 @@ package com.example.tutoapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.FirebaseDatabase
@@ -11,6 +12,9 @@ import kotlinx.android.synthetic.main.activity_tutor.*
 class TutorActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
     private lateinit var uid:String
+
+    var toolbar : Toolbar? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tutor)
@@ -24,5 +28,13 @@ class TutorActivity : AppCompatActivity() {
             startActivity(Intent(this,AtributesActivity::class.java))
             finish()
         }
+
+        toolbar = findViewById(R.id.toolbar)
+        toolbar?.setTitle("")
+        setSupportActionBar(toolbar)
+
+        var actionBar = supportActionBar
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+
     }
 }
