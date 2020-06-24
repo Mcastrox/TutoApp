90b36bf35522e13ce614ac0602d62d2cb7511df5
adripleitez <00067218@uca.edu.sv>
2020-06-23 20:58:40 -0600

    agregando funcionalidad a cambiar password

--- app/src/main/java/com/example/tutoapp/Change_password.kt
+++ app/src/main/java/com/example/tutoapp/Change_password.kt
@@ -2,16 +2,45 @@ package com.example.tutoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.storage.FirebaseStorage
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_profile.*
+import kotlinx.android.synthetic.main.activity_register.*
 
 class Change_password : AppCompatActivity() {
 
+    private lateinit var auth: FirebaseAuth
+    private lateinit var etchange_pass: EditText
+    private lateinit var etnew_pass: EditText
+    private lateinit var etconfirm_pass: EditText
 
     var toolbar : Toolbar? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_change_password)
+
+        etchange_pass = findViewById(R.id.pass_change)
+        etnew_pass = findViewById(R.id.pass_new)
+        etconfirm_pass = findViewById(R.id.confirm_pass)
+
+
+        modificar_info.setOnClickListener {
+            update()
+        }
 
         toolbar = findViewById(R.id.toolbar)
         toolbar?.setTitle(R.string.change_password)
@@ -22,4 +51,42 @@ class Change_password : AppCompatActivity() {
 
 
     }
+
+    private fun update() {
+        val user = FirebaseAuth.getInstance().currentUser
+
+        val password: String = etchange_pass.text.toString()
+        val new: String = etnew_pass.text.toString()
+        val confirm: String = etconfirm_pass.text.toString()
+
+
+        if (etnew_pass.text.length < 6 || TextUtils.isEmpty(password) || TextUtils.isEmpty(confirm) || (new != confirm) ) {
+            etnew_pass.setError("La contraseña debe tener al menos 6 caracteres")
+            etchange_pass.setError("No puede ir vacío.")
+            etconfirm_pass.setError("Debe ser igual a la nueva contraseña")
+
+        }
+
+        val newPassword = etnew_pass.text.toString()
+
+
+        if (!TextUtils.isEmpty(password) && !TextUtils.isEmpty(new) && (new == confirm) && (etnew_pass.text.length >= 6))
+            user?.updatePassword(newPassword)
+                ?.addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(this, "Password actualizado", Toast.LENGTH_LONG).show()
+
+                        etchange_pass.text.clear()
+                        etnew_pass.text.clear()
+                        etconfirm_pass.text.clear()
+                    }
+                }
+        else {
+            Toast.makeText(this, "Por favor revise todos los campos", Toast.LENGTH_LONG).show()
+        }
+
+    }
+
 }
+
+
