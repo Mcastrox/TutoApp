48b205cf697e7c02aac8d2397b10b9038403af39
adripleitez <00067218@uca.edu.sv>
2020-07-04 23:19:40 -0600

    arreglando vistas

--- app/src/main/java/com/example/tutoapp/adapter/SolicitudAdapter.kt
+++ app/src/main/java/com/example/tutoapp/adapter/SolicitudAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import androidx.core.content.ContextCompat
 import com.example.tutoapp.R
 import com.example.tutoapp.models.TutoriaModel
 import com.squareup.picasso.Picasso
@@ -22,6 +23,14 @@ class SolicitudAdapter(
         layout.apellido_estudiante.text = tutoria.apellido_estudiante
         layout.ubicacion.text = tutoria.direccion
         layout.estado.text = tutoria.estado
+
+        if(tutoria.estado.equals("En espera")){
+            layout.estado.setTextColor(ContextCompat.getColor(mContext, R.color.yellow))
+        }else if (tutoria.estado.equals("Rechazada")){
+            layout.estado.setTextColor(ContextCompat.getColor(mContext, R.color.redBrick))
+        }else if (tutoria.estado.equals("Aceptada")){
+            layout.estado.setTextColor(ContextCompat.getColor(mContext, R.color.green))
+        }
 
 
         Picasso.get().load(tutoria.foto_estudiante).into(layout.foto_estudiante)
--- app/src/main/java/com/example/tutoapp/adapter/SolicitudEnviadaAdapter.kt
+++ app/src/main/java/com/example/tutoapp/adapter/SolicitudEnviadaAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import androidx.core.content.ContextCompat
 import com.example.tutoapp.R
 import com.example.tutoapp.models.TutoriaModel
 import com.squareup.picasso.Picasso
@@ -23,6 +24,13 @@ class SolicitudEnviadaAdapter(
         layout.ubicacion.text = tutoria.direccion
         layout.estado.text = tutoria.estado
 
+        if(tutoria.estado.equals("En espera")){
+            layout.estado.setTextColor(ContextCompat.getColor(mContext, R.color.yellow))
+        }else if (tutoria.estado.equals("Rechazada")){
+            layout.estado.setTextColor(ContextCompat.getColor(mContext, R.color.redBrick))
+        }else if (tutoria.estado.equals("Aceptada")){
+            layout.estado.setTextColor(ContextCompat.getColor(mContext, R.color.green))
+        }
 
         Picasso.get().load(tutoria.foto_tutor).into(layout.foto_estudiante)
 
